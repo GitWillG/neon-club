f970058e40a44a0181e9b79b8d5d82da1e3c9389
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-16 21:44:39 -0400

    rename/revert shader change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ import {
   neonClubEmissiveVertexShader,
 } from './shaders/neonEmissive.js';
 import { sphereFragment, sphereVertex } from './shaders/sphere.js';
-// import { cloudFragment, cloudVertex } from './shaders/clouds.js';
+import { cloudFragment, cloudVertex } from './shaders/clouds.js';
 
 const { useApp, useLoaders, useFrame, useCleanup, usePhysics, useInternals } =
   metaversefile;
@@ -270,10 +270,16 @@ export default (e) => {
   //     vertexColors: true,
   //     uniforms: {
   //       uTime:{value:0},
+  //       uBeat: { value: 0.5 },
+  //       uMood: { value: new THREE.Vector3(0.1, 0.2, 0.6) },
+  //       uResolution: {
+  //          value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+  //       },
+  //       uTexture: { value: null },
   //     }
   //   })
   // )
-  // clouds.position.set(0, 0, 0);
+  // clouds.position.set(83,5,43);
   // clouds.rotation.set(0, 0, 0);
   // clouds.updateMatrixWorld();
 
@@ -473,6 +479,9 @@ export default (e) => {
         //   (moodChangerColor[1] + beatFactor1 / 22) / 5,
         //   (moodChangerColor[2] + beatFactor1 / 30) / 5
         // );
+      neonClubEmissiveMaterial.uniforms.uBeat.value = beatFactor1;
+        neonClubCyberLinesMaterial.uniforms.uBeat1.value = beatFactor1;
+        neonClubCyberLinesMaterial.uniforms.uBeat2.value = beatFactor3;
         // sphere.material.uniforms.uBeat.value = beatFactor3;
       }
       if (beatFactor2) {
@@ -507,9 +516,6 @@ export default (e) => {
     };
     if (beatSpeakerBass){
       reactWoofer = beatSpeakerBass;
-      neonClubEmissiveMaterial.uniforms.uBeat.value = beatSpeakerBass;
-      neonClubCyberLinesMaterial.uniforms.uBeat1.value = beatSpeakerBass;
-      neonClubCyberLinesMaterial.uniforms.uBeat2.value = beatSpeakerBass;
       // console.log(reactMid);
     };
     // if (speakers){
