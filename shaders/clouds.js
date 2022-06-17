0eefcce2d3808ac3d7957da69f3232202a3d0f09
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-16 21:43:35 -0400

    renamed shader

---
+++ shaders/clouds.js
@@ -0,0 +1,17 @@
+import * as THREE from 'three'
+const cloudVertex = ``
+
+
+const cloudFragment = `
+
+varying vec3 vPattern;
+varying vec2 vUv;
+uniform vec2 uResolution;
+uniform vec3 uMood;
+uniform float uTime;
+uniform float uBeat;
+`
+
+
+
+export { neonClubCyberLinesVertex, neonClubCyberLinesFragment }
